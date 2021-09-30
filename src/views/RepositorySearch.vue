f3d92c0e51303e9d1add98693d13adcf62c445e6
Mehmet Tuna <mehmet_tuna_anadolu@hotmail.com>
2021-10-01 00:19:43 +0300

    Update RepositorySearch.vue
    added header in repository page

--- src/views/RepositorySearch.vue
+++ src/views/RepositorySearch.vue
@@ -1,48 +1,78 @@
 <template>
   <div class="container mx-auto">
-    <div class="max-content py-4 sticky top-0">
-      <div class="bg-white flex items-center rounded-full shadow-xl">
-        <input
+    <div class="max-content py-4">
+      <div class="mt-1 flex flex-col items-center justify-center">
+        <div
           class="
-            rounded-l-full
-            w-full
-            py-1
-            px-2
-            text-gray-700
-            leading-tight
-            focus:outline-none
+            mx-auto
+            flex-shrink-0 flex
+            items-center
+            justify-center
+            h-12
+            w-12
+            rounded-full
+            mb-2
           "
-          v-model="searchText"
-          id="search"
-          type="text"
-          placeholder="Search"
-          @keypress.enter="search"
-        />
-        <div class="p-1">
-          <button
-            class="
-              bg-blue-500
-              text-white
-              rounded-full
-              p-2
-              hover:bg-blue-400
-              focus:outline-none
-              w-10
-              h-10
-              flex
-              items-center
-              justify-center
-            "
-            @click="search"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-              <path
-                fill="currentColor"
-                d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"
-              ></path>
-            </svg>
-          </button>
+        >
+          <img :src="require('@/assets/github.svg')" />
         </div>
+        <h3 class="text-lg leading-6 font-medium text-gray-900 mb-2">
+          Github search public repos
+        </h3>
+      </div>
+    </div>
+    <div
+      class="
+        max-content
+        bg-white
+        flex
+        items-center
+        rounded-full
+        shadow-xl
+        sticky
+        top-2
+      "
+    >
+      <input
+        class="
+          rounded-l-full
+          w-full
+          py-1
+          px-2
+          text-gray-700
+          leading-tight
+          focus:outline-none
+        "
+        v-model="searchText"
+        id="search"
+        type="text"
+        placeholder="Search"
+        @keypress.enter="search"
+      />
+      <div class="p-1">
+        <button
+          class="
+            bg-blue-500
+            text-white
+            rounded-full
+            p-2
+            hover:bg-blue-400
+            focus:outline-none
+            w-10
+            h-10
+            flex
+            items-center
+            justify-center
+          "
+          @click="search"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+            <path
+              fill="currentColor"
+              d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
     <div class="max-content overflow-y-auto" id="listedItemBody">
